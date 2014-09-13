7597fe87ff4671e5ecd3566181921474aebd6ccd
Sebastian Österlund <sirmcx@gmail.com>
2014-09-13 17:10:57 +0200

    Added line number to error messages.

--- check_cstd.py
+++ check_cstd.py
@@ -36,17 +36,18 @@ for i in subprocess.check_output(["find", ".", "-name", "*.[ch]"]).decode('utf-8
     contents = f.read()
     f.close()
 
-    if max((len(s) for s in contents.split('\n'))) > 80:
-        die(i + ": line exceeding 80 characters")
+    for n, line in enumerate(contents.split('\n'), start=1):
+        if len(line) > 80:
+            die(i + ":" + str(n) + ": line exceeding 80 characters")
 
-    if re.search('[\v\r\t\f]', contents):
-        die(i + ": invalid whitespace character in source")
+        if re.search('[\v\r\t\f]', contents):
+            die(i + ":" + str(n) + ": invalid whitespace character in source")
 
-    if re.search('[^\n\x20-\x7e]', contents):
-        die(i + ": non-printable character in source")
+        if re.search('[^\n\x20-\x7e]', contents):
+            die(i + ":" + str(n) + ": non-printable character in source")
 
-    if re.search(' $', contents, re.M):
-        die(i + ": trailing whitespace")
+        if re.search(' $', contents, re.M):
+            die(i + ":" + str(n) + ": trailing whitespace")
 
     # remove comments and replace strings/characters
     canon = re.sub(r'/\*([^*]|\*[^/])*\*/', '', contents)
@@ -54,53 +55,53 @@ for i in subprocess.check_output(["find", ".", "-name", "*.[ch]"]).decode('utf-8
     canon = re.sub(r'"([^\\]|\\.)*"', 'str', canon)
     canon = re.sub(r"(')(?:(?=(\\?))\2.)*?\1", 'chr', canon)
 
-    for m in re.finditer(r'^ *# *define +([a-zA-Z_][a-zA-Z0-9_]*)', canon, re.M):
-        if re.search('[a-z]', m.group(1)):
-            die(i + ": macro name is not all caps")
+    for n, line in enumerate(canon.split('\n'), start=1):
+        for m in re.finditer(r'^ *# *define +([a-zA-Z_][a-zA-Z0-9_]*)', canon, re.M):
+            if re.search('[a-z]', m.group(1)):
+                die(i + ":" + str(n) + ": macro name is not all caps")
 
-    for m in re.finditer(r'^ *# *define +[^ (]+\(([^)]*)\)', canon, re.M):
-        for a in (s.strip() for s in m.group(1).split(',')):
-            if a != a.capitalize():
-                die(i + ": macro argument is not capitalized")
+        for m in re.finditer(r'^ *# *define +[^ (]+\(([^)]*)\)', canon, re.M):
+            for a in (s.strip() for s in m.group(1).split(',')):
+                if a != a.capitalize():
+                    die(i + ":" + str(n) + ": macro argument is not capitalized")
 
-    for m in re.finditer(r'^ *(typedef +)?(struct|enum|union) +(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)', canon, re.M):
-        if re.search('[A-Z]', m.group('id')):
-            die(i + ": bad struct, enum or union name")
+        for m in re.finditer(r'^ *(typedef +)?(struct|enum|union) +(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)', canon, re.M):
+            if re.search('[A-Z]', m.group('id')):
+                die(i + ":" + str(n) + ": bad struct, enum or union name")
 
-    if re.search(r'\b(if|for|while|do|(__)?asm(__)?|return|continue|break|sizeof)\b([^ ;]|  +)', canon, re.M):
-        die(i + ": keyword not followed by single space or semicolon")
+        if re.search(r'\b(if|for|while|do|(__)?asm(__)?|return|continue|break|sizeof)\b([^ ;]|  +)', canon, re.M):
+            die(i + ":" + str(n) + ": keyword not followed by single space or semicolon")
 
-    if re.search(';([^ \n]|  +)', canon, re.M):
-        die(i + ": semicolon not at end of line and not followed by exactly one space")
+        if re.search(';([^ \n]|  +)', canon, re.M):
+            die(i + ":" + str(n) + ": semicolon not at end of line and not followed by exactly one space")
 
-    if re.search(r'[^ ] +[];)]', canon):
-        die(i + ": semicolon or closing )/] separated from previous token with a space")
+        if re.search(r'[^ ] +[];)]', canon):
+            die(i + ":" + str(n) + ": semicolon or closing )/] separated from previous token with a space")
 
-    if re.search(r'[[(] +', canon):
-        die(i + ": opening (/[ separated from next token with a space")
+        if re.search(r'[[(] +', canon):
+            die(i + ":" + str(n) + ": opening (/[ separated from next token with a space")
 
-    if re.search(r'( (\+\+|--|[~!]) )|([^ ([](\+\+|--|[~!])[^] ;)])|(= +\* +)|([^ ([&]&[^] &)])', canon):
-        die(i + ": improperly spaced operator")
+        if re.search(r'( (\+\+|--|[~!]) )|([^ ([](\+\+|--|[~!])[^] ;)])|(= +\* +)|([^ ([&]&[^] &)])', canon):
+            die(i + ":" + str(n) + ": improperly spaced operator")
 
-    for line in canon.split('\n'):
         if re.match(r' *# *include', line): continue
 
         m = re.search(r'([]a-zA-Z0-9_]+) +\(', line)
         if m and m.group(1) not in ["if", "for", "while", "do", "return", "continue", "break", "sizeof"] \
            and not re.match(r' *# *define', line):
-            die(i + ": incorrect space between cast or function and opening '('")
+               die(i + ":" + str(n) + ": incorrect space between cast or function and opening '('")
 
         if re.search(r'[^ !+*\/%^&=<>|-](&=|\*=|-=|\+=|-=|\+=|&&|\||\|\||[\/%:?^=]|[<>=]=|<<?|>>?|!=?|^)', line):
-            die(i + ": missing space before operator")
+            die(i + ":" + str(n) + ": missing space before operator")
 
         if re.search(r'[^-](&&|\||\|\||[/%:?^=]|[<>=]=|<<?|>>?)[^ /%:?^=><|]', line):
-            die(i + ": missing space after operator")
+            die(i +  ":" + str(n) + ": missing space after operator")
 
         if re.search(r'(,)[^ ]', line):
-            die(i + ": missing space after comma")
+            die(i + ":" + str(n) + ": missing space after comma")
 
         if re.search(r'-> | ->', line):
-            die(i + ": invalid space around ->")
+            die(i + ":" + str(n) + ": invalid space around ->")
 
 
     for m in re.finditer(r'^( +)', canon, re.M):
@@ -111,14 +112,14 @@ for i in subprocess.check_output(["find", ".", "-name", "*.[ch]"]).decode('utf-8
 
     # brace alignment
     dstack = []
-    for row, line in enumerate(canon.split('\n')):
+    for row, line in enumerate(canon.split('\n'), start=1):
         for col, c in enumerate(line):
             if c == '{': dstack.append(col)
             if c == '}':
                 if len(dstack) is 0:
-                    die(i + ": closing } without opening {")
+                    die(i + ":" + str(row) +": closing } without opening {")
                 elif col != dstack.pop():
-                    die(i + ": closing } not aligned with opening {")
+                    die(i + ":" + str(row) + ": closing } not aligned with opening {")
 
 # check spacing across all files
 if spacing:
